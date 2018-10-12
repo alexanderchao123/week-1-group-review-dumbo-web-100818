6f141dbfd68db8b759b314e7e51de2d979ce1c01
github username <chao.alexander.123@gmail.com>
2018-10-12 11:23:51 -0400

    create methods to the questions

--- review-question-1.rb
+++ review-question-1.rb
@@ -1,65 +1,86 @@
 ## QUESTION 1
+require 'pry'
 
-pokemon = [
+def pokemons
+  pokemon = [
+    {
+      "id": 1,
+      "name": "bulbasaur",
+      "base_experience": 64,
+      "height": 7,
+      "is_default": true,
+      "order": 1,
+      "weight": 69,
+      "abilities": [
+          {
+              "is_hidden": true,
+              "slot": 3,
+              "ability": {
+                  "name": "chlorophyll",
+                  "url": "http://pokeapi.co/api/v2/ability/34/"
+              }
+          }
+      ]
+  },
   {
-    "id": 1,
-    "name": "bulbasaur",
-    "base_experience": 64,
-    "height": 7,
+    "id": 3,
+    "name": "venesaur",
+    "base_experience": 50,
+    "height": 10,
     "is_default": true,
     "order": 1,
-    "weight": 69,
+    "weight": 90,
     "abilities": [
         {
             "is_hidden": true,
             "slot": 3,
             "ability": {
-                "name": "chlorophyll",
-                "url": "http://pokeapi.co/api/v2/ability/34/"
+                "name": "fire",
+                "url": "http://pokeapi.co/api/v2/ability/38/"
             }
         }
     ]
-},
-{
-  "id": 3,
-  "name": "venesaur",
-  "base_experience": 50,
-  "height": 10,
-  "is_default": true,
-  "order": 1,
-  "weight": 90,
-  "abilities": [
-      {
-          "is_hidden": true,
-          "slot": 3,
-          "ability": {
-              "name": "fire",
-              "url": "http://pokeapi.co/api/v2/ability/38/"
-          }
-      }
+  },
+  {
+    "id": 2,
+    "name": "pikachu",
+    "base_experience": 60,
+    "height": 4,
+    "is_default": true,
+    "order": 1,
+    "weight": 37,
+    "abilities": [
+        {
+            "is_hidden": true,
+            "slot": 3,
+            "ability": {
+                "name": "lightning",
+                "url": "http://pokeapi.co/api/v2/ability/30/"
+            }
+        }
+    ]
+  }
   ]
-},
-{
-  "id": 2,
-  "name": "pikachu",
-  "base_experience": 60,
-  "height": 4,
-  "is_default": true,
-  "order": 1,
-  "weight": 37,
-  "abilities": [
-      {
-          "is_hidden": true,
-          "slot": 3,
-          "ability": {
-              "name": "lightning",
-              "url": "http://pokeapi.co/api/v2/ability/30/"
-          }
-      }
-  ]
-}
-]
+end
 
+# binding.pry
+def get_url(pokemon_name)
+  pokemons.each do |character|
+    if character[:name] == pokemon_name
+      return character[:abilities][0][:ability][:url]
+    end
+  end
+end
+
+puts get_url("bulbasaur")
+
+def base_experience(base_num)
+  pokemons.find do |character|
+    character[:base_experience] > 40
+  end
+end
+
+puts base_experience(40)
 
 # How would you get the url for Bulbasaur's ability?
 # How would you return the first pokemon with base experience over 40?
